f4fbc2f5b840f4e0cf35cfce5b2dd7b2e65ea57e
wr3dny <pawel.pawtel@gmail.com>
2025-10-24 23:11:50 +0200

    Button more

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,12 @@ import styles from './Button.module.css'
 interface Props {
     label: string
     type: 'navbar' | 'navbar-lego'
+    onClick?: () => void
 }
 
-export const Button = ({label, type}:Props) => {
+export const Button = ({label, type, onClick}:Props) => {
     return (
-        <div className={classNames(styles[type])}>
+        <div className={classNames(styles[type])} onClick={onClick}>
             {label}
         </div>
     )
