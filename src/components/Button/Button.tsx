57baa35cca9e0634d031c7ed217d27bf8f197323
wr3dny <pawel.pawtel@gmail.com>
2025-10-26 00:19:24 +0200

    Button more

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,9 +5,20 @@ interface Props {
     label: string
     type: 'navbar' | 'navbar-lego'
     onClick?: () => void
+    insideBorder?: boolean
 }
 
-export const Button = ({label, type, onClick}:Props) => {
+export const Button = ({label, type, onClick, insideBorder}:Props) => {
+    if(insideBorder){
+        return (
+            <div className={classNames(styles[type], styles['outer'])}>
+            <div className={classNames(styles[type])} onClick={onClick}>
+                {label}
+            </div>
+            </div>
+        )
+    }
+
     return (
         <div className={classNames(styles[type])} onClick={onClick}>
             {label}
