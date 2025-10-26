4ce36cdea7beb087978240dd1c44ef59026e1b57
wr3dny <pawel.pawtel@gmail.com>
2025-10-26 16:12:49 +0100

    Added variables colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,24 +3,14 @@ import styles from './Button.module.css'
 
 interface Props {
     label: string
-    type: 'navbar' | 'navbar-lego'
+    size: 'small' | 'medium' | 'large' | 'round'
     onClick?: () => void
-    insideBorder?: boolean
 }
 
-export const Button = ({label, type, onClick, insideBorder}:Props) => {
-    if(insideBorder){
-        return (
-            <div className={classNames(styles[type], styles['outer'])}>
-            <div className={classNames(styles[type])} onClick={onClick}>
-                {label}
-            </div>
-            </div>
-        )
-    }
+export const Button = ({label,size,  onClick}:Props) => {
 
     return (
-        <div className={classNames(styles[type])} onClick={onClick}>
+        <div className={classNames(styles[size])} onClick={onClick}>
             {label}
         </div>
     )
