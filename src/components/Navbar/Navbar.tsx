de7450ca346b6c805aadf6ef04e53a04656f4782
wr3dny <pawel.pawtel@gmail.com>
2025-11-19 23:00:20 +0100

    Lego items mask

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,29 @@
-import {NavButton} from "./NavButton/NavButton.tsx";
-import styles from './Navbar.module.css'
-import {PATHS} from "../../const/paths.ts";
-import {useLocation} from "react-router-dom";
-import {NavLogo} from "./NavLogo/NavLogo.tsx";
+import { NavButton } from "./NavButton/NavButton.tsx";
+import styles from "./Navbar.module.css";
+import { PATHS } from "../../const/paths.ts";
+import { useLocation } from "react-router-dom";
+import { NavLogo } from "./NavLogo/NavLogo.tsx";
 
 export const Navbar = () => {
-    const currentPath = useLocation();
-    const strippedPath = currentPath.pathname.startsWith("/") ? currentPath.pathname.slice(1) : currentPath.pathname;
-    const uppercasePath = strippedPath.toUpperCase()
-    const displayPath = uppercasePath || "HOME"
+  const currentPath = useLocation();
+  const strippedPath = currentPath.pathname.startsWith("/")
+    ? currentPath.pathname.slice(1)
+    : currentPath.pathname;
+  const uppercasePath = strippedPath.toUpperCase();
+  const displayPath = uppercasePath || "HOME";
 
-    return (
-        <div className={styles.container}>
-            <NavLogo />
-            <div className={styles.non_logo}>
-                <div className={styles.path}>{displayPath}            </div>
-                <div className={styles.buttons}>
-                    {Object.entries(PATHS).map(([name, path]) => (
-                        <NavButton key={name} label={name} to={path} />
-                    ))}
-                </div>
-                <div className={styles.hidden} />
-            </div>
+  return (
+    <div className={styles.container}>
+      <NavLogo />
+      <div className={styles.non_logo}>
+        <div className={styles.path}>{displayPath} </div>
+        <div className={styles.buttons}>
+          {Object.entries(PATHS).map(([name, path]) => (
+            <NavButton key={name} label={name} to={path} />
+          ))}
         </div>
-    )
-}
+        <div className={styles.hidden} />
+      </div>
+    </div>
+  );
+};
--- src/pages/Lego/Lego.tsx
+++ src/pages/Lego/Lego.tsx
@@ -1,5 +1,34 @@
-export const Lego = () => {
-    return (
-        <div>Lego</div>
-    )
+import { LegoWishlist } from "../../const/lego.ts";
+import styles from "./Lego.module.css";
+
+interface Lego {
+  setNumber: number;
+  url: string;
+  image: string;
+  alt: string;
 }
+
+export const Lego = () => {
+  const sortedList = [...LegoWishlist].sort((a, b) => a.price - b.price);
+
+  return (
+    <div className={styles.page}>
+      <h4>Lista życzeń</h4>
+      <div className={styles.item}>
+        {sortedList.map((item: Lego) => (
+          <a
+            key={item.setNumber}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.card}
+          >
+            <div className={styles.mask}>
+              <img src={item.image} alt={item.alt} />
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
