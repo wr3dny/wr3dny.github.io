8fadb7b11cf93e36cf2e5ef542428b7db141217e
wr3dny <pawel.pawtel@gmail.com>
2025-11-22 23:49:50 +0100

    Lego again

--- src/components/Navbar/NavButton/NavButton.stories.tsx
+++ src/components/Navbar/NavButton/NavButton.stories.tsx
@@ -1,8 +1,8 @@
 import { MemoryRouter } from "react-router-dom";
-import {NavButton} from "./NavButton.tsx";
+import { NavButton } from "./NavButton.tsx";
 
 export const Basic = () => (
-    <MemoryRouter initialEntries={["/"]}>
-        <NavButton label="Home" to={'/'} key='one' />
-    </MemoryRouter>
+  <MemoryRouter initialEntries={["/"]}>
+    <NavButton label="Home" to={"/"} key="one" disabled={false} />
+  </MemoryRouter>
 );
