b3bc12ff55fab16ca36d1db178de2b9fc1fe2b05
wr3dny <pawel.pawtel@gmail.com>
2026-01-20 19:08:51 +0100

    Using API on books

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,37 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
+import { useEffect, useState } from "react";
+
+type BuildInfo = {
+  commitSha: string;
+  commitDate: string;
+  commitMessage: string;
+};
 
 export const Footer = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    )
-}
+  const [info, setInfo] = useState<BuildInfo | null>(null);
+
+  useEffect(() => {
+    fetch("/build-info.json")
+      .then((r) =>
+        r.ok ? r.json() : Promise.reject(new Error("No build info")),
+      )
+      .then(setInfo)
+      .catch(() => setInfo(null));
+  }, []);
+
+  const today = new Date().toISOString().split("T")[0];
+
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.hidden} />
+      <div className={styles.display}>
+        {!info ? (
+          <div>No info</div>
+        ) : (
+          <div>Last update - date - description</div>
+        )}
+        <div>{today}</div>
+      </div>
+    </footer>
+  );
+};
